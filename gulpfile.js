d95f60c6cdce36f51c515c6cff68ab6e92ddf095
Jonathan Richardson <jon@Jonathans-MacBook-Pro.local>
2015-10-08 00:29:50 -0500

    Add version bumping to gulp configuration.

--- gulpfile.js
+++ gulpfile.js
@@ -11,21 +11,30 @@ var gulp         = require('gulp'),
     cache        = require('gulp-cache'),
     livereload   = require('gulp-livereload'),
     replace      = require('gulp-replace'),
-    pkg          = require('./package.json'),
+    bump         = require('gulp-bump'),
     del          = require('del');
 
+gulp.task('bump', function(){
+  return gulp.src(['package.json'])
+  .pipe(bump())
+  .pipe(gulp.dest('./'));
+});
 
 gulp.task('clean', function(cb) {
   del(['dist']);
   cb();
 });
 
-gulp.task('js', ['clean'], function() {
+gulp.task('js', ['clean', 'bump'], function() {
+  var fs = require('fs')
+  var json = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+  var version = json.version;
+
   return gulp.src('src/**/*.js')
     .pipe(concat('classify.js'))
 
     //Update the version number from the source file
-    .pipe(replace('{{VERSION}}',pkg.version)) 
+    .pipe(replace('{{VERSION}}', version)) 
 
     .pipe(gulp.dest('dist'))
     .pipe(rename({suffix: '.min'}))
