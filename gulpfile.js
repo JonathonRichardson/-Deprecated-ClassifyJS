7a5fe51c9367736066a471619fde266aa6242426
Jonathan Richardson <jon@Jonathans-MacBook-Pro.local>
2015-10-07 22:05:11 -0500

    Convert repo from using brunch to use gulp.

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+gulp.task('clean', function(cb) {
+  del(['dist']);
+  cb();
+});
+
+gulp.task('js', ['clean'], function() {
+  return gulp.src('src/**/*.js')
+    .pipe(concat('classify.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('default', ['js']);
